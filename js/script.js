cb72d75a85ad3d61e15c6daedf177496f35fd581
Airodragon <airodragon1919@gmail.com>
2023-03-14 16:47:22 +0530

    homepage_redesign

--- js/script.js
+++ js/script.js
@@ -11,3 +11,22 @@ window.addEventListener('mousemove', function(e) {
 //     parallaxBg.style.transform = 'translate3d(0px, ' + yPos + 'px, 0px)';
 //   });
   
+
+$(document).ready(function(){
+    $(".testimonial .indicators li").click(function(){
+      var i = $(this).index();
+      var targetElement = $(".testimonial .tabs li");
+      targetElement.eq(i).addClass('active');
+      targetElement.not(targetElement[i]).removeClass('active');
+              });
+              $(".testimonial .tabs li").click(function(){
+                  var targetElement = $(".testimonial .tabs li");
+                  targetElement.addClass('active');
+                  targetElement.not($(this)).removeClass('active');
+              });
+          });
+  $(document).ready(function(){
+      $(".slider .swiper-pagination span").each(function(i){
+          $(this).text(i+1).prepend("0");
+      });
+  });
